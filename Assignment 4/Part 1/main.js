e3e62a7aa5b8d3b9732e6381e338dca24269d17f
Liam Matthews <55813762+Toast732@users.noreply.github.com>
2024-04-04 20:15:51 -0400

    A4P1: - Added the variables and functions for the first part.

--- Assignment 4/Part 1/main.js
+++ Assignment 4/Part 1/main.js
@@ -5,3 +5,50 @@
 	Working with javascript.
 */
 
+
+/*
+
+
+	Constants
+
+
+*/
+
+// Get the custom name attribute from the document.
+const customNameInput = document.getElementById('customname');
+
+// Get the generate button from the document.
+const generateButton = document.querySelector('.randomize');
+
+// Get the story output field from the document.
+const storyOutputField = document.querySelector('.story');
+
+// Store the text for the story.
+const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+
+// Store the options for insertx.
+const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas", "Pootis"]
+
+// Store the options for inserty.
+const insertY = ["the soup kitchen", "Disneyland", "the White House", "Your house"]
+
+// Store the options for insertz.
+const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away", "ate a sandvich"]
+
+/*
+
+
+	Functions
+
+
+*/
+
+// Function for getting a random value from an array.
+function randomValueFromArray(array){
+	// Get the random index to get from the array.
+	const random_index = Math.floor(Math.random() * array.length);
+
+	// Return the random value from the array.
+	return array[random_index];
+}
+
